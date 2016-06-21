f9cd0aa81d39bb4c99daa91f0f115ed8477a6640
tyler <tyler@DEV-MacMini8.local>
2016-06-21 15:14:26 -0700

    animates pointer to follow mouse

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 $(document).ready(function () {
 	
-	/*visible canvas area*/
+	/* visible canvas area */
 	GameCanvas = function () {
 			this.canvas = document.createElement('canvas');
 			this.context = this.canvas.getContext('2d');
 			this.grid = new GameGrid();
+			this.queue = queueHandler();
+
+			function queueHandler() {
+				var queue = []
+			}
 	};
 
 	/* populates canvas with visible orbs */
@@ -24,30 +29,47 @@ $(document).ready(function () {
 
 	GameCanvas.prototype.start = function () {
 		this.canvas.setAttribute('id', 'canvas');
-		this.canvas.width = 465;
+		this.canvas.width = 460;
 		this.canvas.height = 600;
 		
 		$('#canvas-area').html(this.canvas);
 	};
 
-	GameCanvas.prototype.drawAimer = function () {
+	GameCanvas.prototype.drawAimer = function (mouseCoords) {
+		var x = mouseCoords[0],
+			y = mouseCoords[1],
+			aimerX = 230,
+			aimerY = 600,
+			mouseAngle = radToDeg(Math.atan2((aimerY+15) - y, x - (aimerX+15)));
+
+		if (mouseAngle < 0) {
+        	mouseAngle = 180 + (180 + mouseAngle);
+    	}	
+
+		function radToDeg(angle) {
+		    return angle * (180 / Math.PI);
+		}
+
+		function degToRad(angle) {
+    		return angle * (Math.PI / 180);
+		}
+
+		context.clearRect(205,560,50,50);
 		this.context.beginPath();
-		this.context.moveTo(260, 550);
-		this.context.lineTo(250, 600);
-		this.context.lineTo(270, 600);
-		context.fillStyle = 'orange';
-	    context.fill();
-	    context.lineWidth = 2;
-	    context.strokeStyle = '#003300';
-		this.context.closePath();	
+		context.lineWidth = 3;
+		context.strokeStyle = "green";	
+		this.context.moveTo(aimerX, aimerY-15);
+		this.context.lineTo(aimerX + 1.5 * 15 * Math.cos(degToRad(mouseAngle)),
+						   (aimerY - 15) - 1.5 * 15 * Math.sin(degToRad(mouseAngle))); 
+		this.context.stroke();	
 	};
 
-	/* grid to store orbs and locations */
+	/* array grid to store orb data and locations */
 	GameGrid = function () {
 		this.grid = []; //[row][col]
 	};
 
-	/*populates grid with orbs*/
+	/* populates grid with orbs */
 	GameGrid.prototype.fillGrid = function () {
 		this.rows = 15;
 		this.cols = 15;
@@ -96,13 +118,15 @@ $(document).ready(function () {
 	};
 //						TO-DO:
 //-------------------------------------------------------
+//make pointer follow mouse.
+//add orb to end of pointer
+//shoot orb
+//collision detection
+//add orb to queue position to the side of the pointer 
 //calculate individual orbs position
-//function for randomizing orb color
-//function for filling the board with randommized orbs
 //fill grid according to how many orbs fit in a row
 //calculate how many orbs fit in a row
 //at start of game fill 5 rows with orbs
-//offset every other row (orb.diameter/2)
 
 	//start game! 
 	$('#start-button').click(function () {
@@ -111,24 +135,24 @@ $(document).ready(function () {
 		gameCanvas.start();
 		gameCanvas.grid.fillGrid();
 		gameCanvas.fillCanvas();
-		gameCanvas.drawAimer();
 		mouseCoordsOnCanvas();
 		
 		//returns current mouse X and Y relative to canvas
 		function mouseCoordsOnCanvas() {
 			var canvasTop  = gameCanvas.canvas.getBoundingClientRect().top,
-				canvasLeft = gameCanvas.canvas.getBoundingClientRect().left;
+				canvasLeft = gameCanvas.canvas.getBoundingClientRect().left,
+				canvasX, canvasY, mouseCoords = 1;
 
-			$(document).on('mousemove', function (event) {
-				console.log(event.pageX - canvasLeft, event.pageY - canvasTop);
+			$('#canvas').on('mousemove', getMouseCoords);
+			
+			function getMouseCoords (event) {
 				
-				var canvasX = event.pageX - canvasLeft,
-					canvasY = event.pageY - canvasTop,
-					mouseCoords = [canvasX, canvasY];
-
-				return mouseCoords;
-			})	
-						 	
+				canvasX = event.pageX - canvasLeft;
+				canvasY = event.pageY - canvasTop;
+				mouseCoords = [canvasX, canvasY];
+				gameCanvas.drawAimer(mouseCoords); 
+			};	
+	
 		}
 	});
 })
